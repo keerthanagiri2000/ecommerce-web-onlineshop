1b438dd585e23bc24329ebdd4ccb089a8e2e1e37
keerthanagiri2000 <keerthanagiri2000@gmail.com>
2022-08-12 17:47:41 +0530

    same repo

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -26,8 +26,8 @@ export const Home = () => {
             <h3 className='text-center'>Our Products</h3>
             {/* our products */}
                 <div className="d-flex justify-content-center flex-wrap">
-                    {ourProducts.map((product) => (
-                        <ProductCard {...product} />
+                    {ourProducts.map((product, index) => (
+                        <ProductCard {...product} key={index} />
                     ))}
                 </div>
                 <div>
@@ -38,8 +38,8 @@ export const Home = () => {
         <div className='recent-products-container container mt-4'>
                <h3 className='text-center'>Categories</h3>
                <Row>
-                {categories.map((category) => (
-                    <LinkContainer to={`/category/${category.name.toLocaleLowerCase()}`}>
+                {categories.map((category, index) => (
+                    <LinkContainer key={index} to={`/category/${category.name.toLocaleLowerCase()}`}>
                      <Col md={4}>
                         <div style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${category.image})`, gap: '10px'}} className='category-tile'>
                           {category.name}
--- Frontend/src/services/appApi.js
+++ Frontend/src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
  
 export const appApi = createApi ({
     reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://mernshop-ecommerce.herokuapp.com"}),
     endpoints: (builder) => ({
 
         // signup
